d54309ea26cea3aec1fe9a4ed75228287289c85a
Dallas Blackwell <mdvayne923@gmail.com>
2020-05-16 00:51:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,16 +15,3 @@ end
   # k_v_arr = hash.first
   # min_key = k_v_arr[0]
   # min_val = k_v_arr[0]
-  
-# def key_for_min_value(name_hash)
-#   lowest_value = 0
-#   lowest_key = nil
-  
-# name_hash.each do |key,value|
-# if lowest_value == 0 || value < lowest_value
-#       lowest_value = value 
-#       lowest_key = key
-#     end
-#   end
-#   lowest_key
-# end
